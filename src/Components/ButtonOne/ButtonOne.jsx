5ded1d26a5d01d3039d8eef9f2399c35bf029019
tonmoy <tonmoyth13@gmail.com>
2025-07-13 21:37:57 +0600

    updated our partners section and popular courses section

--- src/Components/ButtonOne/ButtonOne.jsx
+++ src/Components/ButtonOne/ButtonOne.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ButtonOne = ({level}) => {
   return (
     <button
-      class=" rounded px-5 py-2.5 overflow-hidden group bg-primary relative hover:bg-gradient-to-r hover:bg-primary hover:primary text-white hover:ring-2 hover:ring-offset-2 hover:ring-primary transition-all ease-out duration-300"
+      class=" rounded px-5 py-2.5 overflow-hidden group bg-primary relative hover:bg-gradient-to-r hover:bg-primary hover:primary hover:cursor-pointer text-white hover:ring-2 hover:ring-offset-2 hover:ring-primary transition-all ease-out duration-300"
     >
       <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
       <span class="relative">{level}</span>
--- src/Pages/Home/Partners/PartnersSection.jsx
+++ src/Pages/Home/Partners/PartnersSection.jsx
@@ -11,15 +11,15 @@ const PartnersSection = () => {
   }, []);
 
   return (
-    <section className="py-16 bg-base-100">
-      <div className="container mx-auto px-4 text-center">
-        <h2 className="text-3xl font-bold mb-6">Our Trusted Partners</h2>
-        <p className="text-gray-600 mb-10 max-w-2xl mx-auto">
-          We collaborate with world-class brands to drive innovation in online
-          education.
+    <section className="py-10 lg:py-12 ">
+      <div className=" text-center">
+        <h2 className="text-3xl font-bold mb-2">Our Partners</h2>
+        <p className=" mb-4 max-w-2xl mx-auto">
+          We are proud to work with leading organizations who support our
+          mission to educate and empower.
         </p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 text-[var(--text)]">
           {partners.map((partner) => (
             <div
               key={partner.id}
@@ -29,10 +29,12 @@ const PartnersSection = () => {
                 <img
                   src={partner.logo}
                   alt={partner.name}
-                  className="h-16 w-auto object-contain"
+                  className="h-16 w-auto  object-contain  filter 
+             invert-0 brightness-0 
+             dark:invert dark:brightness-100"
                 />
               </div>
-              <p className="text-gray-600 text-sm">{partner.description}</p>
+              <p className=" text-sm">{partner.description}</p>
             </div>
           ))}
         </div>
